4dd9853e4a62adf5749fe18d5ce73a27a906df8a
Benjamin Beuscart <benjamin.beuscart@isen.yncrea.fr>
2020-01-27 15:36:39 +0100

    Initializer

---
+++ lab04-web/src/main/java/Initializer.java
@@ -0,0 +1,28 @@
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import yncrea.lab04.core.config.AppConfig;
+import yncrea.lab04.core.config.DBConfig;
+
+public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        Class[] configClasses = new Class[2];
+        configClasses[0] = AppConfig.class;
+        configClasses[1] = DBConfig.class;
+        return configClasses;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        Class[] servletConfigClasses = new Class[1];
+        servletConfigClasses[0] = WebConfig.class;
+        return servletConfigClasses;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        String[] servletMappings = new String[1];
+        servletMappings[0] = "/";
+        return servletMappings;
+    }
+}
