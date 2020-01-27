5b6eeedf706c5b926ee547377a4032fc79aa075f
Benjamin Beuscart <benjamin.beuscart@isen.yncrea.fr>
2020-01-27 17:16:38 +0100

    CompanyController
    BDD vide, la remplir avec Application.java du lab03

--- lab04-web/src/main/java/yncrea/lab04/web/controller/CompanyController.java
+++ lab04-web/src/main/java/yncrea/lab04/web/controller/CompanyController.java
@@ -1,4 +1,22 @@
 package yncrea.lab04.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import yncrea.lab04.core.service.CompanyService;
+
+@Controller
 public class CompanyController {
+
+    private CompanyService companyService;
+
+    public CompanyController(final CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @RequestMapping("/list")
+    public String getListOfCompanies(ModelMap modelMap) {
+        modelMap.put("companies", companyService.findAllWithProjects());
+        return "companiesList";
+    }
 }
