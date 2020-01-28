0bb3578ea0ed163055a64b564458f468bef32b1c
Benjamin Beuscart <benjamin.beuscart@isen.yncrea.fr>
2020-01-28 23:15:06 +0100

    CompanyController Part3
    trash button + beginning of logs

--- lab04-web/src/main/java/yncrea/lab04/web/controller/CompanyController.java
+++ lab04-web/src/main/java/yncrea/lab04/web/controller/CompanyController.java
@@ -2,14 +2,22 @@ package yncrea.lab04.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import yncrea.lab04.core.entity.Company;
 import yncrea.lab04.core.service.CompanyService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class CompanyController {
 
     private CompanyService companyService;
+
+    private static final Logger logger = LoggerFactory.getLogger(CompanyController.class);
 
     public CompanyController(final CompanyService companyService) {
         this.companyService = companyService;
@@ -18,18 +26,28 @@ public class CompanyController {
     @RequestMapping("/list")
     public String getListOfCompanies(ModelMap modelMap) {
         modelMap.put("companies", companyService.findAllWithProjects());
+        logger.debug("List of companies displayed.");
         return "companiesList";
     }
 
     @RequestMapping(value = "/form")
     public String getForm(ModelMap modelMap) {
         modelMap.addAttribute("company", new Company());
+        logger.debug("Company adding form.");
         return "companyForm";
     }
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
     public String submitForm(@ModelAttribute("company") Company company) {
         companyService.save(company);
+        logger.debug("Company added.");
         return "redirect:list";
+    }
+
+    @RequestMapping(value = "{id}/delete", method = RequestMethod.GET)
+    public String deleteCompany(@PathVariable long id) {
+        companyService.deleteById(id);
+        logger.debug("Company deleted, ID : ", id, " .");
+        return "redirect:/list";
     }
 }
